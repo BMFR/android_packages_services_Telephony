582665f47eab3431848ab652f4e3af7f886bbe58
martincz <martincz@mokeedev.com>
2014-10-08 13:58:20 +0800

    merge upstream

--- src/com/android/phone/MSimCallFeaturesSetting.java
+++ src/com/android/phone/MSimCallFeaturesSetting.java
@@ -146,9 +146,6 @@ public class MSimCallFeaturesSetting extends CallFeaturesSetting {
 
         mSubManager = SubscriptionManager.getInstance();
 
-        // get buttons
-        PreferenceScreen prefSet = getPreferenceScreen();
-
         mButtonXDivert = (PreferenceScreen) findPreference(BUTTON_XDIVERT_KEY);
 
         PreferenceScreen selectSub = (PreferenceScreen) findPreference(BUTTON_SELECT_SUB_KEY);
@@ -170,7 +167,6 @@ public class MSimCallFeaturesSetting extends CallFeaturesSetting {
                     Settings.System.ALLOW_CALL_RECORDING, false));
         }
 
-        removeOptionalPrefs(prefSet);
     }
 
 
@@ -297,8 +293,9 @@ public class MSimCallFeaturesSetting extends CallFeaturesSetting {
     @Override
     protected void removeOptionalPrefs(PreferenceScreen preferenceScreen) {
         super.removeOptionalPrefs(preferenceScreen);
-        if (!getResources().getBoolean(R.bool.config_show_xdivert) && mButtonXDivert != null) {
-            preferenceScreen.removePreference(mButtonXDivert);
+        if (!getResources().getBoolean(R.bool.config_show_xdivert)) {
+            Preference xdivert = findPreference(BUTTON_XDIVERT_KEY);
+            preferenceScreen.removePreference(xdivert);
         }
     }
 
